40e1737dbe63216b6b539ff633ecbbf232f1ed44
Robert Boyd <bert.vern.boyd@gmail.com>
2015-03-11 21:47:49 -0230

    Added root route

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   end
 
   resources :users
-
+  
+  root :to => "sessions#new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
